5d4d723b9d384dd39a64d036d0ff96c9acebddd5
Matias0Fernandez <135881964+Matias0Fernandez@users.noreply.github.com>
2024-06-18 08:57:02 -0300

    Delete Desktop/Anima Cafe/anima-cafe directory

--- Desktop/Anima Cafe/anima-cafe/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import { TablaProductos } from "./components/TablaProductos";
-import { PRODUCTOS } from "./components/Productos";
-
-export const App = () => {
-  return <TablaProductos productos={PRODUCTOS} />;
-};
